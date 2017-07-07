d68b1df271372f3a31d3ad5958254c69d3226039
wj_java_xuqian <455205595@qq.com>
2017-07-07 17:55:29 +0800

    新建web项目

---
+++ src/main/java/com/xuqian/gs/web/App.java
@@ -0,0 +1,13 @@
+package com.xuqian.gs.web;
+
+/**
+ * Hello world!
+ *
+ */
+public class App 
+{
+    public static void main( String[] args )
+    {
+        System.out.println( "Hello World!" );
+    }
+}
